7e8b7dcc3a5493f79decc8098513822e1b1e6842
YoungKnight337 <warfire9145@gmail.com>
2025-10-31 17:31:35 -0400

    Small Change

--- Stealth/Enemy.h
+++ Stealth/Enemy.h
@@ -7,6 +7,7 @@ class Enemy: public CharacterController
 private:
 	int height = 50;
 	int width = 50;
+	float lineofSight;
 	Vector2 position;
 	Color color = RED;
 public:
@@ -14,6 +15,7 @@ public:
 	~Enemy();
 	void Update(float deltaTime);
 	void Draw();
+	void Follow();
 
 };
 
