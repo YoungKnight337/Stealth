76a570050f515ed66c2875bd01869c89b82a78fa
YoungKnight337 <warfire9145@gmail.com>
2025-10-31 20:38:51 -0400

    Small Change

--- Stealth/Enemy.cpp
+++ Stealth/Enemy.cpp
@@ -1,1 +1,32 @@
 #include "Enemy.h"
+
+
+Enemy::Enemy()
+{
+
+}
+
+Enemy::~Enemy()
+{
+
+}
+
+void Enemy::Draw()
+{
+	DrawRectangle(position.x, position.y, width, height, color);
+}
+
+void Enemy::Update(float deltaTime)
+{
+	Move();
+}
+
+void Enemy::Move()
+{
+
+}
+
+void Enemy::Follow()
+{
+
+}
