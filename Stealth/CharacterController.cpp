30cb4637b40844f22e52a4584e01f47b99d33c85
YoungKnight337 <warfire9145@gmail.com>
2025-11-04 09:21:13 -0500

    Small Change

--- Stealth/CharacterController.cpp
+++ Stealth/CharacterController.cpp
@@ -22,3 +22,19 @@ void CharacterController::Movement()
 {
 
 }
+
+void CharacterController::Damage(int health)
+{
+
+}
+
+void CharacterController::Shoot()
+{
+
+}
+
+void CharacterController::Slash()
+{
+
+
+}
--- Stealth/CharacterController.h
+++ Stealth/CharacterController.h
@@ -18,6 +18,7 @@ public:
 	void Movement();
 	void Damage(int health);
 	void Shoot();
+	void Slash();
 
 };
 
