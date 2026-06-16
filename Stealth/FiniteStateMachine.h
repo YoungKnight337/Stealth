d8e8c640d3660240aeea2d899ff68f582de983ce
YoungKnight337 <warfire9145@gmail.com>
2026-06-16 09:05:45 -0400

    Bug Fixing

--- Stealth/FiniteStateMachine.h
+++ Stealth/FiniteStateMachine.h
@@ -13,6 +13,7 @@ private:
 public:
 	FiniteStateMachine();
 	~FiniteStateMachine();
+	void Enter(Enemy* enemy);
 	void Update(Enemy& enemy, float deltaTime);
 	void InitializeStates(Enemy& enemy);
 	void StateTransition(State* newState);
